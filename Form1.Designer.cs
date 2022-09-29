fb2c5a41f1ceda358858633fcc84a0e0a608feb6
Руслан Козляк <rkozliak@gmail.com>
2022-09-29 19:28:12 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace QR_Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultTextBox = new System.Windows.Forms.RichTextBox();
+            this.CroppedPicBox = new System.Windows.Forms.PictureBox();
+            this.PDFPicBox = new System.Windows.Forms.PictureBox();
+            this.DecodeButton = new System.Windows.Forms.Button();
+            this.AutoScanButton = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.OpenFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CroppedPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PDFPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(632, 389);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(396, 272);
+            this.resultTextBox.TabIndex = 1;
+            this.resultTextBox.Text = "";
+            // 
+            // CroppedPicBox
+            // 
+            this.CroppedPicBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.CroppedPicBox.Location = new System.Drawing.Point(632, 12);
+            this.CroppedPicBox.Name = "CroppedPicBox";
+            this.CroppedPicBox.Size = new System.Drawing.Size(396, 319);
+            this.CroppedPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CroppedPicBox.TabIndex = 2;
+            this.CroppedPicBox.TabStop = false;
+            // 
+            // PDFPicBox
+            // 
+            this.PDFPicBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.PDFPicBox.Location = new System.Drawing.Point(12, 45);
+            this.PDFPicBox.Name = "PDFPicBox";
+            this.PDFPicBox.Size = new System.Drawing.Size(600, 616);
+            this.PDFPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PDFPicBox.TabIndex = 0;
+            this.PDFPicBox.TabStop = false;
+            this.PDFPicBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PDFPicBox_Paint);
+            this.PDFPicBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PDFPicBox_MouseDown);
+            this.PDFPicBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PDFPicBox_MouseMove);
+            // 
+            // DecodeButton
+            // 
+            this.DecodeButton.Location = new System.Drawing.Point(632, 337);
+            this.DecodeButton.Name = "DecodeButton";
+            this.DecodeButton.Size = new System.Drawing.Size(167, 46);
+            this.DecodeButton.TabIndex = 3;
+            this.DecodeButton.Text = "Сканировать код";
+            this.DecodeButton.UseVisualStyleBackColor = true;
+            this.DecodeButton.Click += new System.EventHandler(this.DecodeButton_Click);
+            // 
+            // AutoScanButton
+            // 
+            this.AutoScanButton.Location = new System.Drawing.Point(805, 337);
+            this.AutoScanButton.Name = "AutoScanButton";
+            this.AutoScanButton.Size = new System.Drawing.Size(223, 46);
+            this.AutoScanButton.TabIndex = 4;
+            this.AutoScanButton.Text = "Сканировать автоматически";
+            this.AutoScanButton.UseVisualStyleBackColor = true;
+            this.AutoScanButton.Click += new System.EventHandler(this.AutoScanButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.OpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Location = new System.Drawing.Point(12, 10);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(600, 29);
+            this.OpenFile.TabIndex = 5;
+            this.OpenFile.Text = "Открыть PDF файл";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 673);
+            this.Controls.Add(this.OpenFile);
+            this.Controls.Add(this.AutoScanButton);
+            this.Controls.Add(this.DecodeButton);
+            this.Controls.Add(this.CroppedPicBox);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.PDFPicBox);
+            this.Name = "Form1";
+            this.Text = "PDF QR Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CroppedPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PDFPicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox resultTextBox;
+        private System.Windows.Forms.PictureBox CroppedPicBox;
+        private System.Windows.Forms.PictureBox PDFPicBox;
+        private System.Windows.Forms.Button DecodeButton;
+        private System.Windows.Forms.Button AutoScanButton;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.Button OpenFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using O2S.Components.PDFRender4NET;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZXing;
+
+namespace QR_Scanner
+{
+    public partial class Form1 : Form
+    {
+        private IBarcodeReader barcodeReader = new BarcodeReader();
+
+        private Point selectionAreaStartPoint;
+        private Rectangle selectionArea = new Rectangle();
+
+        private Brush selectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
+
+        private double hRatio = 0;
+        private double wRatio = 0;
+
+        private Bitmap pageImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            resultTextBox.Clear();
+            CroppedPicBox.Image = null;
+
+            if (OpenFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = OpenFileDialog.FileName;
+
+            //Открываем полученный PDF файл
+            PDFFile pdfFile = PDFFile.Open(filename);
+            //Преобразуем его в изображение (Bitmap)
+            pageImage = pdfFile.GetPageImage(0, 300);
+            PDFPicBox.Image = pageImage;
+            //Рассчитываем разность в координатах между пикчербоксом и
+            //оригинальным изображением, чтобы можно было преобразовать
+            //координаты для отрисовки вырезанного изображения.
+            //Это возникает из за установленного поля SizeMode = StretchImage
+            hRatio = (double)pageImage.Height / PDFPicBox.Height;
+            wRatio = (double)pageImage.Width / PDFPicBox.Width;
+        }
+
+        private void PDFPicBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Задаем начальную точку области выделения для вырезки ищоражения
+            selectionAreaStartPoint = e.Location;
+        }
+
+        private void PDFPicBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Пока не отжата левая кнопака перерисовываем область выделения
+            if (e.Button != MouseButtons.Left)
+                return;
+            Point tempEndPoint = e.Location;
+            selectionArea.Location = new Point(
+                Math.Min(selectionAreaStartPoint.X, tempEndPoint.X),
+                Math.Min(selectionAreaStartPoint.Y, tempEndPoint.Y));
+            selectionArea.Size = new Size(
+                Math.Abs(selectionAreaStartPoint.X - tempEndPoint.X),
+                Math.Abs(selectionAreaStartPoint.Y - tempEndPoint.Y));
+            PDFPicBox.Invalidate();
+        }
+
+        private void PDFPicBox_Paint(object sender, PaintEventArgs e)
+        {
+            //Иент возникающий при каждом движении мышкой по пикчербоксу из за вызова
+            //метода PDFPicBox.Invalidate(), который заставляет пикчер бок перерисовываться
+            try
+            {
+                if (PDFPicBox.Image != null)
+                {
+                    if (selectionArea.Width > 0 && selectionArea.Height > 0)
+                    {
+                        e.Graphics.FillRectangle(selectionBrush, selectionArea);
+
+                        //Пересчитываем в пропорциях координаты для вырезки изображения
+                        selectionArea.X = (int)(selectionArea.X * wRatio);
+                        selectionArea.Y = (int)(selectionArea.Y * hRatio);
+                        selectionArea.Width = (int)(selectionArea.Width * wRatio);
+                        selectionArea.Height = (int)(selectionArea.Height * hRatio);
+
+                        var img = (Bitmap)PDFPicBox.Image;
+                        //С помощью метода Clone можно удобно вырезать изображение
+                        //перекопировав только прямоугольную его часть из SelectionArea
+                        var crop = img.Clone(selectionArea, img.PixelFormat);
+                        CroppedPicBox.Image = crop;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Не выходите за границы!");
+            }
+
+        }
+
+        private void DecodeButton_Click(object sender, EventArgs e)
+        {
+            //Декодируем QR код, который содержится в CroppedPicBox
+            if (CroppedPicBox.Image != null)
+            {
+                //Получаем Bitmap изображения из CroppedPicBox
+                var QR = (Bitmap)CroppedPicBox.Image;
+                //Используем метод для декодирования QR кода из библиотеки Zxing
+                var result = barcodeReader.Decode(QR);
+                if (result != null)
+                    resultTextBox.Text = result.Text;
+                else
+                    resultTextBox.Text = "Неудается распознать QR код!";
+            }
+            else
+                MessageBox.Show("Сначала выделите QR код или откройте PDF файл!");
+
+        }
+
+        private void AutoScanButton_Click(object sender, EventArgs e)
+        {
+            //Второй вариант получения QR кода
+            //Работает только для данного тестового документа, 
+            //так как только здесь мы знаем где находится QR код
+            if (pageImage != null)
+            {
+                var rect = new Rectangle(new Point(100, 100), new Size(300, 300));
+                var QR = pageImage.Clone(rect, pageImage.PixelFormat);
+                CroppedPicBox.Image = QR;
+                var result = barcodeReader.Decode(QR);
+                if (result != null)
+                    resultTextBox.Text = result.Text;
+                else
+                    resultTextBox.Text = "Неудается распознать QR код!";
+            }
+            else
+                MessageBox.Show("Сначала откройте PDF файл!");
+
+        }
+    }
+}
